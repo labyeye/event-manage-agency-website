e2fb1041178fb3086a1b418d06768a49811da43e
Labh Bothra <labhbothra1380@gmail.com>
2026-02-06 21:36:32 +0530

    Done

--- scripts.js
+++ scripts.js
@@ -2,14 +2,14 @@ let isMobileNavOpen = false;
 
 function toggleMobileNav() {
   isMobileNavOpen = !isMobileNavOpen;
-  const navLinks = document.querySelector('.nav-links');
-  navLinks.classList.toggle('active', isMobileNavOpen);
+  const navLinks = document.querySelector(".nav-links");
+  navLinks.classList.toggle("active", isMobileNavOpen);
 }
 
 document.addEventListener("DOMContentLoaded", () => {
-  const navLinksContainer = document.querySelector('.nav-links');
+  const navLinksContainer = document.querySelector(".nav-links");
 
-  navLinksContainer.querySelectorAll("a").forEach(link => {
+  navLinksContainer.querySelectorAll("a").forEach((link) => {
     link.addEventListener("click", () => {
       if (isMobileNavOpen) {
         navLinksContainer.classList.remove("active");
